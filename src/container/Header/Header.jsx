82768f793753522a63c03474efa683332170ac51
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-10-20 18:07:47 +0300

    navbar

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { images } from '../../constants/images';
+
+
+import './Header.css';
+
+const Header = () => (
+  <div classname="app__header app__wrapper section_padding" id="home">
+    <div classname="app__wrapper_info">
+      <SubHeading/>
+
+    </div>
+    <div clasname="app__wrapperr_img">
+
+    </div>
+    
+  </div>
+);
+
+export default Header;
